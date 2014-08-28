e9ef416e41cf438ebf6483daa00c407a9a07c683
Zlac <lizhi890611@gmail.com>
2014-08-28 01:48:15 -0700

    Passcal II problem

--- Passcal2.h
+++ Passcal2.h
@@ -4,7 +4,7 @@ public:
         vector<int> res(rowIndex + 1, 0);
         for(int i = 0; i <= rowIndex; i++) {
             res[i] = 1;
-            for(int j = i - 1; j > 0; j--) res[j] = res[j] + res[j - 1];
+            for(int j = i - 1; j > 0; j--) res[j] += res[j - 1];
             }
         return res;
     }
