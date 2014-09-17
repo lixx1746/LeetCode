1b6860af22fda1d736c065d27dbcd94ca627dc7a
Zlac <lizhi890611@gmail.com>
2014-09-16 20:52:29 -0700

    climb stair

---
+++ singleNumber2.h
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        int ans = 0, count = 0;
+        int bit = 1;
+        for(int i = 0; i < 32; i++) {
+            for(int j = 0; j < n; j++) {
+                if(A[j] & bit) count++;
+            }
+            if(count % 3) ans |= bit;
+            bit <<= 1;
+            count = 0;
+        }
+        return ans;
+    }
+};
