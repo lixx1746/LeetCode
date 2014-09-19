bfbd364cf72aaf45f178e175a3849961b7e4d37b
Zlac <lizhi890611@gmail.com>
2014-09-18 23:26:03 -0700

    easy question

---
+++ minWindow.h
@@ -0,0 +1,22 @@
+class Solution {
+    int find[256], need[256];
+public:
+    string minWindow(string S, string T) {
+        int ns = S.size(), nt = T.size();
+        for(int i = 0; i < nt; i++) need[T[i]]++;
+        int flag = 0, l = 0, r = 0, count = 0, ans_len = ns;
+        string ans = "";
+        for(; ;) {
+            for(; r < ns && count < nt; r++) if(need[S[r]] > find[S[r]]++) count++;
+            flag = count == nt;
+            for(; l < r && count == nt; l++) if(need[S[l]] && --find[S[l]] < need[S[l]]) count--;
+            if(flag  && r - l + 1 <= ans_len) {
+                ans_len = r - l + 1;
+                ans = S.substr(l - 1, ans_len);
+                flag = 0;
+            }
+            if(r == ns) break;
+        }
+        return ans;
+    }
+};
