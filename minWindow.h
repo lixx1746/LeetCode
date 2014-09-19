96d0a4c8eeb60d3e5f170faf2eec108c8de4e676
Zlac <lizhi890611@gmail.com>
2014-09-18 23:35:02 -0700

    min window using two pointer

--- minWindow.h
+++ minWindow.h
@@ -4,16 +4,15 @@ public:
     string minWindow(string S, string T) {
         int ns = S.size(), nt = T.size();
         for(int i = 0; i < nt; i++) need[T[i]]++;
-        int flag = 0, l = 0, r = 0, count = 0, ans_len = ns;
+        int flag = 0, count = 0, ans_len = ns;
         string ans = "";
-        for(; ;) {
+        for(int r = 0, l = 0; ;flag = 0) {
             for(; r < ns && count < nt; r++) if(need[S[r]] > find[S[r]]++) count++;
             flag = count == nt;
             for(; l < r && count == nt; l++) if(need[S[l]] && --find[S[l]] < need[S[l]]) count--;
             if(flag  && r - l + 1 <= ans_len) {
                 ans_len = r - l + 1;
                 ans = S.substr(l - 1, ans_len);
-                flag = 0;
             }
             if(r == ns) break;
         }
