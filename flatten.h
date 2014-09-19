2cad457691f2d97d56e11109417de48d0f67b768
Zlac <lizhi890611@gmail.com>
2014-09-19 02:14:01 -0700

    non-rec way, very beautiful way

---
+++ flatten.h
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        while(root) {
+            if(root->left) {
+                TreeNode *p = root->left;
+                while(p->right) p = p->right;
+                p->right = root->right;
+                root->right = root->left;
+                root->left = NULL;
+            }
+            root = root->right;
+        }
+
+    }
+};
