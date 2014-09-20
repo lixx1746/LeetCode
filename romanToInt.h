d1d1602b08d301f8741897f74fffe47ade7f615b
Zlac <lizhi890611@gmail.com>
2014-09-20 12:24:21 -0700

    roman to int

---
+++ romanToInt.h
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.size();
+        if(n == 0) return 0;
+        map<char, int> mp;
+        mp['I'] = 1,  mp['V'] = 5, mp['X'] = 10, mp['L'] = 50;
+        mp['C'] = 100, mp['D'] = 500, mp['M'] = 1000;
+        int sum = mp[s[n - 1]];
+        for(int i = n - 2; i >= 0; i--) {
+            if(mp[s[i]] < mp[s[i + 1]]) sum -= mp[s[i]];
+            else sum += mp[s[i]];
+        }
+        return sum;
+    }
+};
