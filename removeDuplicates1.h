3db7ee4427f4bd953e880ad54b949d82d20469b5
Zlac <lizhi890611@gmail.com>
2014-09-19 19:53:49 -0700

    easy question

---
+++ removeDuplicates1.h
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
+        if(n == 0) return 0;
+        int len = 1;
+        for(int i = 1; i < n; i++) {
+            if(A[len - 1] != A[i]) A[len++] = A[i];
+        }
+        return len;
+    }
+};
